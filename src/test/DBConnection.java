859d6fd1f8860113f661dfd4c5b4b2e064549621
Shashi Raj <shashiraj.972@gmail.com>
2019-02-04 14:27:08 +0530

    Initial Commit

---
+++ src/test/DBConnection.java
@@ -0,0 +1,19 @@
+package test;
+import java.sql.*;
+public class DBConnection {
+	private static Connection con;
+	private DBConnection() {};
+	static {
+		try {
+		Class.forName("oracle.jdbc.driver.OracleDriver");
+		con = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe","SHASHI","MANAGER");
+		}//End of try
+		
+		catch(Exception e) {}
+	}//End of Static block
+	
+	public static Connection getCon()
+	{
+		return con;
+	}
+}
